c136c4c8226b38e82f1a548289da7b3880ae600b
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-03-29 20:18:00 +0300

    Fix binary search

--- src/recursive_binary_search.py
+++ src/recursive_binary_search.py
@@ -1,31 +1,26 @@
-import math
+def recursive_binary_search(items, search_item, low=0, high=None):
+    """Recursive binary search"""
+    if high is None:
+        high = len(items) - 1
+    if low > high:
+        return None
 
-
-def recursive_binary_search(items, search_item):
-    low = 0
-    high = len(items) - 1
-
-    mid = math.ceil((low + high) / 2)
+    mid = (low + high) // 2
 
     guess = items[mid]
 
     if guess == search_item:
         return mid
     elif guess > search_item:
-        return low + recursive_binary_search(items[:mid], search_item)
-    elif guess < search_item:
-        return high - 1 + recursive_binary_search(items[mid + 1:], search_item)
+        return recursive_binary_search(items, search_item, low, mid - 1)
     else:
-        return None
+        return recursive_binary_search(items, search_item, mid + 1, high)
 
 
 # testing part
 search_list = [1, 3, 5, 7, 9]
 
-print(recursive_binary_search(search_list, 1))
-print(recursive_binary_search(search_list, 7))
-
 for index, item in enumerate(search_list):
     assert recursive_binary_search(search_list, item) == index
 
-# assert recursive_binary_search(search_list, -1) is None
+assert recursive_binary_search(search_list, -1) is None
